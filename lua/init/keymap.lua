087555767f626167aefdfbac197282ede71a7d57
flareseek <flareseek@gmail.com>
2025-01-10 11:40:29 +0900

    configs

--- lua/init/keymap.lua
+++ lua/init/keymap.lua
@@ -1,18 +1,39 @@
-vim.g.mapleader = ","
+-- vim.g.mapleader = " "
+local vscode = require('vscode')
 
 -- > cursor
-vim.keymap.set('n', 'j', function() vim.cmd('call VSCodeCall("cursorDown")') end, { silent = true })
-vim.keymap.set('n', 'k', function() vim.cmd('call VSCodeCall("cursorUp")') end, { silent = true })
+vim.keymap.set('n', 'j', function()
+  local count = vim.v.count == 0 and 1 or vim.v.count
+  vscode.call("cursorMove", {
+    args = {
+      to = "down",
+      by = "wrappedLine",
+      value = count,
+    }
+  })
+end, { silent = true })
+vim.keymap.set('n', 'k', function()
+  local count = vim.v.count == 0 and 1 or vim.v.count
+  vscode.call("cursorMove", {
+    args = {
+      to = "up",
+      by = "wrappedLine",
+      value = count,
+    }
+  })
+end, { silent = true })
+-- vim.keymap.set('n', 'j', 'gj', { silent = true })
+-- vim.keymap.set('n', 'k', 'gk', { silent = true })
 -- >
 
 -- > fold
-vim.keymap.set('n', 'zM', function() vim.cmd('call VSCodeNotify("editor.foldAll")') end, { silent = true })
-vim.keymap.set('n', 'zR', function() vim.cmd('call VSCodeNotify("editor.unfoldAll")') end, { silent = true })
-vim.keymap.set('n', 'zc', function() vim.cmd('call VSCodeNotify("editor.fold")') end, { silent = true })
-vim.keymap.set('n', 'zC', function() vim.cmd('call VSCodeNotify("editor.foldRecursively")') end, { silent = true })
-vim.keymap.set('n', 'zo', function() vim.cmd('call VSCodeNotify("editor.unfold")') end, { silent = true })
-vim.keymap.set('n', 'zO', function() vim.cmd('call VSCodeNotify("editor.unfoldRecursively")') end, { silent = true })
-vim.keymap.set('n', 'za', function() vim.cmd('call VSCodeNotify("editor.toggleFold")') end, { silent = true })
+vim.keymap.set('n', 'zM', function() vscode.call("editor.foldAll") end, { silent = true })
+vim.keymap.set('n', 'zR', function() vscode.call("editor.unfoldAll") end, { silent = true })
+vim.keymap.set('n', 'zc', function() vscode.call("editor.fold") end, { silent = true })
+vim.keymap.set('n', 'zC', function() vscode.call("editor.foldRecursively") end, { silent = true })
+vim.keymap.set('n', 'zo', function() vscode.call("editor.unfold") end, { silent = true })
+vim.keymap.set('n', 'zO', function() vscode.call("editor.unfoldRecursively") end, { silent = true })
+vim.keymap.set('n', 'za', function() vscode.call("editor.toggleFold") end, { silent = true })
 -- >
 
 -- > explorer
